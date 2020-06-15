b6c86481901816a9e9ebe23ce1788d8aea7ee724
reedhop24 <40578449+reedhop24@users.noreply.github.com>
2020-06-14 17:50:45 -0700

    Update app.js

--- app.js
+++ app.js
@@ -11,43 +11,42 @@ app.use(upload());
 
 app.post('/upload', async (req, res) => {
 
-    let importedSheet = (XLSX.read(req.files.file.data, {type:'buffer'}));
-    let sheet = importedSheet.Sheets['Company Totals'];
-    let toJson = XLSX.utils.sheet_to_json(sheet);
+    var importedSheet = (XLSX.read(req.files.file.data, {type:'buffer'}));
+    var sheet = importedSheet.Sheets['Company Totals'];
+    var toJson = XLSX.utils.sheet_to_json(sheet);
     var AgencyName = [];
     var AgencyTotalQuote = [];
     var AgencyAppliedQuote = [];
     var branchName = ['Agency Totals'];
-    let company = [];
-    let quote = [];
-    let apply = [];
+    var company = [];
+    var quote = [];
+    var apply = [];
     // Have to push the end HTML into an array, so I have loaded the CSS as the first element then the HTML will come in after
-    let barArray = [fs.readFileSync(__dirname + '/public/main.html')];
-    //For some reason inside of the generator function i is recognized as the length of the object, so I had to create a seperate counter variable
-    let count = 0;
+    var barArray = [fs.readFileSync(__dirname + '/public/main.html')];
+    var count = 0;
     
     try{
 
-        for(var i = 1; i < toJson.length; i++) {
+        for(let i = 1; i < toJson.length; i++) {
 
             var agencyArr = (toJson[i]['Agency Totals:'].split(' '));
 
-            if(agencyArr[0] !== 'Companies' && agencyArr[0] !== 'Branch:'){
+            if(agencyArr[0] !== 'Companies' && agencyArr[0] !== 'Branch:') {
                 AgencyName.push(agencyArr[0]);
             }
 
-            if(agencyArr[0] === 'Branch:'){
+            if(agencyArr[0] === 'Branch:') {
                 branchName.push(agencyArr.join(''));
             }
 
-            if(toJson[i]['__EMPTY'] !== 'Quote' && toJson[i]['__EMPTY'] !== undefined){
+            if(toJson[i]['__EMPTY'] !== 'Quote' && toJson[i]['__EMPTY'] !== undefined) {
                 AgencyTotalQuote.push(toJson[i]['__EMPTY']);
             }
 
-            if(toJson[i]['__EMPTY_1'] !== 'Apply' && toJson[i]['__EMPTY_1'] !== undefined){
+            if(toJson[i]['__EMPTY_1'] !== 'Apply' && toJson[i]['__EMPTY_1'] !== undefined) {
                 AgencyAppliedQuote.push(toJson[i]['__EMPTY_1']);
             }
-            if(agencyArr[0] === 'Branch:'){
+            if(agencyArr[0] === 'Branch:') {
                 company.push(AgencyName);
                 quote.push(AgencyTotalQuote);
                 apply.push(AgencyAppliedQuote);
@@ -60,8 +59,8 @@ app.post('/upload', async (req, res) => {
             quote.push(AgencyTotalQuote);
             apply.push(AgencyAppliedQuote);
 
-        for(var i = 0; i < company.length; i++){
-            let x = co(function * () {
+        for(let i = 0; i < company.length; i++) {
+            var x = co(function * () {
                 var data = {
                     labels: company[i],
                     series: [
@@ -83,7 +82,7 @@ app.post('/upload', async (req, res) => {
 
             (async function asyncCall() {
                 var result = await x;
-                if(result === undefined && count === company.length-1){
+                if(result === undefined && count === company.length-1) {
                     res.send(barArray.join(''));
                 }
                 count++;
@@ -91,7 +90,7 @@ app.post('/upload', async (req, res) => {
 
         }
 
-    }catch(err) {
+    } catch(err) {
         console.log(err);
     }
     
